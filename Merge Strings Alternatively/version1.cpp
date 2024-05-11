24d88f984bf74c7e3388f9eda23b5e38ea3a8661
Jason910315 <a0938692163@gmail.com>
2024-05-11 15:52:59 +0800

    2024/05/11

---
+++ Merge Strings Alternatively/version1.cpp
@@ -0,0 +1,33 @@
+//We traverse both string and pick each of words,so the time complexity of it is O(n),
+//since n is the length of the longest words
+
+//Space complexity is O(n),
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string result = "";
+        int i = 0; 
+        while(i < word1.length()||i < word2.length()){
+            if(i < word1.length()){
+                result+=word1[i];
+            }
+            if(i < word2.length()){
+                result+=word2[i];
+            }
+            i++;
+        }
+        return result;
+    }
+};
+int main(){
+    string str1 = "abc";
+    string str2 = "pder";
+    Solution s;
+    string result = s.mergeAlternately(str1,str2);
+    cout<<result;
+}
---
+++ Merge Strings Alternatively/version2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution{
+    public:
+     string mergeAlternately(string word1, string word2){
+        if(word1.length()==0){
+            return word1;
+        }
+        if(word2.length()==0){
+            return word2;
+        }
+        int minSize = word1.length();
+        string result;
+        if(word1.length() > word2.length()){
+            minSize = word2.length();
+        }
+        int i;
+        for(i = 0;i < minSize;i++){
+            result+=word1[i];
+            result+=word2[i];
+        }
+        if(word1.length() > word2.length()){
+            for(;i < word1.length();++i){
+                result+=word1[i];
+            }
+        }
+        else{
+            for(;i < word2.length();++i){
+                result+=word2[i];
+            }
+        }
+        return result;
+     }
+};
+
+int main(){
+    string str1 = "abc";
+    string str2 = "pd e";
+    Solution s;
+    string result = s.mergeAlternately(str1,str2);
+    cout<<result;
+}
